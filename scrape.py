80b5f7279d78598944386770bb272d344395b732
Dean B <dean.bird@gmail.com>
2019-03-04 06:05:01 +1000

    Added some error checking due to internet failure

--- scrape.py
+++ scrape.py
@@ -16,48 +16,55 @@ while True:
 	#Get the list of available posts
 	#print("Hold on, getting data from PasteBin!")
 	print("Getting data from: " + scraping_url)
+	try:
+		scrape_data = requests.get(scraping_url)
+		#pprint(scrape_data)
 
-	scrape_data = requests.get(scraping_url)
+		try:
+			#convert to JSON
+			scrape_json = scrape_data.json()
+			#pprint(scrape_json)
 
-	#pprint(scrape_data)
+			for paste in scrape_json:
+				#Creating the details for the text file
+				#variables
+				filename = datetime.utcfromtimestamp(float(paste['date'])).strftime('%Y-%m-%d %H-%M-%S') + "_" + paste['key'] + ".txt"
+				date =  datetime.utcfromtimestamp(float(paste['date'])).strftime('%Y-%m-%d')
+				
+				#Create the folder if not exists
+				if not os.path.isdir(dest_folder + "/" + date + "/"):
+					print("Creating folder: " + date)
+					os.mkdir(dest_folder + "/" + date + "/")
 
-	#convert to JSON
-	scrape_json = scrape_data.json()
-	#pprint(scrape_json)
+				if os.path.exists(dest_folder + "/" + date + "/" + filename):
+					append_write = 'a' # append if already exists
+					print("Skipping " + filename + ", Already exists")
+				else:
+					append_write = 'w' # make a new file if not
+			
+					#Write out the file
+					paste_file = open(dest_folder + "/" + date + "/" + filename, append_write)
+				
+					#Write the Name to the file
+					paste_file.write("Name: " + paste['title'].encode('utf-8').strip())
+					paste_file.write("\n")
+					paste_file.write("Link: " + paste['full_url'].encode('utf-8').strip())
 
-	for paste in scrape_json:
-		#Creating the details for the text file
-		#variables
-		filename = datetime.utcfromtimestamp(float(paste['date'])).strftime('%Y-%m-%d %H-%M-%S') + "_" + paste['key'] + ".txt"
-		date =  datetime.utcfromtimestamp(float(paste['date'])).strftime('%Y-%m-%d')
-		
-		#Create the folder if not exists
-		if not os.path.isdir(dest_folder + "/" + date + "/"):
-			print("Creating folder: " + date)
-			os.mkdir(dest_folder + "/" + date + "/")
-
-		if os.path.exists(dest_folder + "/" + date + "/" + filename):
-			append_write = 'a' # append if already exists
-			print("Skipping " + filename + ", Already exists")
-		else:
-			append_write = 'w' # make a new file if not
+					#Now get the contents of the paste and dump it into the file
+					paste_data = requests.get(paste['scrape_url'])
+					paste_file.write("\n")
+					paste_file.write("\n+----+----+\n")
+					for line in paste_data:
+						paste_file.write(line)	
+					paste_file.write("\n+----+----+")
+					paste_file.close()
+					print(filename + " written")
+		except ValueError:
+			print("Issue with URL, internet may be experiencing issues. Will try again shortly.")
 	
-			#Write out the file
-			paste_file = open(dest_folder + "/" + date + "/" + filename, append_write)
-		
-			#Write the Name to the file
-			paste_file.write("Name: " + paste['title'].encode('utf-8').strip())
-			paste_file.write("\n")
-			paste_file.write("Link: " + paste['full_url'].encode('utf-8').strip())
-
-			#Now get the contents of the paste and dump it into the file
-			paste_data = requests.get(paste['scrape_url'])
-			paste_file.write("\n")
-			paste_file.write("\n+----+----+\n")
-			for line in paste_data:
-				paste_file.write(line)	
-			paste_file.write("\n+----+----+")
-			paste_file.close()
-			print(filename + " written")
+	except requests.exceptions.RequestException as e:
+		print("An error occured. Will try again shortly")
+		print(e)
 	
-	time.sleep(1)
+	#Wait the minimum amount of time
+	time.sleep(2)
